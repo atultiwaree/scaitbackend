c05f69485c99e18a95cf9a04f4b1e4729c357565
Ginie Digital <giniedigitaldeveloper@gmail.com>
2025-01-30 23:39:23 +0530

    incerease

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(cors());
-app.use(bodyParser.json());
-app.use(express.json({limit: '50mb'}));
+app.use(bodyParser.json({ limit: "200mb" }));
+app.use(bodyParser.urlencoded({ limit: "200mb",  extended: true, parameterLimit: 1000000 }));
+
 
 async function generateResultImage(student) {
   const canvas = createCanvas(883, 1280);
